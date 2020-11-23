c6977b837b1784439b048a00090ed381c29bbc74
slag223 <48025172+slag223@users.noreply.github.com>
2020-11-22 19:29:37 -0500

    stage

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import subject, post, replies, like, agree, informative
 # Register your models here.
+
+admin.site.register(subject)
+admin.site.register(post)
+admin.site.register(replies)
+admin.site.register(like)
+admin.site.register(agree)
+admin.site.register(informative)
